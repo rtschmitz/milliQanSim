bac65b3670d83f795f4b7dca180a3370752a1252
Ryan Schmitz <schmitz@ucsb.edu>
2026-06-04 11:05:51 +0100

    Initial GRENDEL commit

---
+++ beamSetupGrendel.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#create local build of sim, configured to generate cosmic muons
+cp inputData/config/particlesMu.ini inputData/config/particles.ini
+. buildsetup.sh
+cp MilliQan.cc.BeamGen MilliQan.cc
+cp CMakeLists.txt.default CMakeLists.txt
+cp include/defaultGrendel/* include/
+cp src/defaultGrendel/*.cc src/
+cd build
+cmake ../
+make -j8
+./MilliQan ../runMac/mcp_novis.mac
